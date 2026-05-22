10ea63b3825ebc4ceb66336e94b3f3771b8f2477
iamphebeay <phebeobafunso@gmal.com>
2026-05-22 11:52:43 +0100

    first commit

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,7 @@ use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/tasks', [TaskController::class, 'index']);
-
 Route::get('/tasks/create', [TaskController::class, 'create']);
-
 Route::post('/tasks', [TaskController::class, 'store']);
-
 Route::get('/tasks/{id}', [TaskController::class, 'show']);
-Route::put(
-'/tasks/{task}/complete',
-[TaskController::class,'markComplete']
-);
+Route::put('/tasks/{task}/complete',[TaskController::class,'markComplete']);
